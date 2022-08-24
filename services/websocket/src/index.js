f9f7cf26bdb4c39ddccea54dec7c5b8af2866631
jmvaliente <valiente_jose2@hotmail.com>
2022-08-24 14:00:21 +0200

    added .env file and config

--- services/websocket/src/index.js
+++ services/websocket/src/index.js
@@ -1,25 +1,28 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
 const { Server } = require('socket.io');
 const http = require('http');
 const mqtt = require('mqtt');
 
-const server = http.createServer(app);
 const PORT = process.env.PORT || 7000
+const TOPIC = process.env.TOPIC
+const SERVER_MQTT = process.env.SERVER_MQTT
+const server = http.createServer(app);
 const httpServer = server.listen(PORT);
 const ioServer = new Server(httpServer)
 console.log('🤘...Websocket service run in port ' + PORT);
 
-const clientMqtt = mqtt.connect('mqtt://test.mosquitto.org')
+const clientMqtt = mqtt.connect(SERVER_MQTT)
 
 ioServer.on('connection', socketServer => {
     console.log('Connect Socket Server')
 })
 
 clientMqtt.on('connect', function () {
-    clientMqtt.subscribe('testtopic/10', error => {
+    clientMqtt.subscribe(TOPIC, error => {
       if (!error) {
-        clientMqtt.publish('testtopic/10', 'Hello mqtt')
+        clientMqtt.publish(TOPIC, 'Hello mqtt')
       }
     })
   })
