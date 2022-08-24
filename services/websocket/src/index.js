55c471795d0949c3fff4d4b2108718a0fad2b5ac
jmvaliente <valiente_jose2@hotmail.com>
2022-08-24 12:37:27 +0200

    added and init MQTT library

--- services/websocket/src/index.js
+++ services/websocket/src/index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const { Server } = require('socket.io');
 const http = require('http');
+const mqtt = require('mqtt');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 7000
 const httpServer = server.listen(PORT);
-const io = new Server(httpServer)
-
+const ioServer = new Server(httpServer)
 console.log('🤘...Websocket service run in port ' + PORT);
+
+const clientMqtt = mqtt.connect('mqtt://test.mosquitto.org')
+
+ioServer.on('connection', socketServer => {
+    console.log('Connect Socket Server')
+})
+
+clientMqtt.on('connect', function () {
+    clientMqtt.subscribe('testtopic/10', error => {
+      if (!error) {
+        clientMqtt.publish('testtopic/10', 'Hello mqtt')
+      }
+    })
+  })
+
+  clientMqtt.on('message', function (topic, message) {
+    console.log(message.toString())
+    ioServer.emit('message',message.toString())
+  })
